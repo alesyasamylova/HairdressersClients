afcae67e18e21aa579a5e86f070ac71862374a70
Alesia Samylova <alemari.samylova44@gmail.com>
2019-09-15 12:43:52 +0300

    server-side was added

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/clients', (req, res) => {
+    res.sendFile(path.join(__dirname + '/clients.html'));
+});
+
+app.get('/clientinfo', (req, res) => {
+    res.sendFile(path.join(__dirname + '/clientinfo.html'));
+});
+
+app.get('/newclient', (req, res) => {
+    res.sendFile(path.join(__dirname + '/newclient.html'));
+});
+
+app.get('/clientvisit', (req, res) => {
+    res.sendFile(path.join(__dirname + '/clientvisit.html'));
+});
+
+app.get('/newvisit', (req, res) => {
+    res.sendFile(path.join(__dirname + '/newvisit.html'));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
