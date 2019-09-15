816ec418bd2f421b813184753bf472c8ed2deb44
Alesia Samylova <alemari.samylova44@gmail.com>
2019-09-15 13:03:49 +0300

    small fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const port = 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
